aa285bfbd5ace8d3a8a94e18efa6f3dafbf78f92
whackamole7 <whackamole7@proton.me>
2025-11-20 13:13:25 +0300

    lottie problem solved with json?url import; PerksBlock adapting for mobile started

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
+import lottie from "astro-integration-lottie";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [
+		lottie(),
+	],
 	vite: {
 		assetsInclude: ["**/*.lottie"],
 	}
