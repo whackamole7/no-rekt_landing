de51622d89321076def32a44bb43212ab2da188e
whackamole7 <whackamole7@proton.me>
2025-11-27 15:30:47 +0300

    HowBlock steps animation disabled when steps are wrapped

--- src/blocks/HowBlock/HowBlock.js
+++ src/blocks/HowBlock/HowBlock.js
@@ -5,21 +5,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
 	const block = document.querySelector(".HowBlock");
+	const MAX_STEPS_HEIGHT = 575;
+	const steps = block.querySelector(".HowBlock__steps");
+	if (steps.scrollHeight > MAX_STEPS_HEIGHT) {
+		return;
+	}
+
 	const tl = gsap.timeline({
 		scrollTrigger: {
 			trigger: block,
-			start: "top 20%",
-			end: "top: -10%",
+			start: "top 30%",
+			end: "top: 0%",
 			scrub: true,
 			invalidateOnRefresh: true
 		}
 	});
 
-	const [card_1, card_2, card_3] = block.querySelectorAll(".HowBlock__step");
-	gsap.set(card_1, { xPercent: 50, rotate: -10 });
-	gsap.set(card_2, { xPercent: 0 });
-	gsap.set(card_3, { xPercent: -50, rotate: 10 });
+	const [step_1, step_2, step_3] = block.querySelectorAll(".HowBlock__step");
+	gsap.set(step_1, { xPercent: 50, rotate: -10 });
+	gsap.set(step_2, { xPercent: 0 });
+	gsap.set(step_3, { xPercent: -50, rotate: 10 });
 
-	tl.to(card_1, { xPercent: 0, rotate: 0, duration: 1, ease: "power2.out" }, 0);
-	tl.to(card_3, { xPercent: 0, rotate: 0, duration: 1, ease: "power2.out" }, 0);
+	tl.to(step_1, { xPercent: 0, rotate: 0, duration: 1, ease: "power2.out" }, 0);
+	tl.to(step_3, { xPercent: 0, rotate: 0, duration: 1, ease: "power2.out" }, 0);
 });
